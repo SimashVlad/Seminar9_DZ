841e6eba7e7c14124ae9e05e9163b34744dbbf91
Vladislav <SimashVlad@yandex.ru>
2022-06-08 20:06:57 +0500

    Task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 
+Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 
+*/
+
+
+
+// Console.Clear();
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = int.Parse(Console.ReadLine());
+    return output;
+}
+
+PrintSum(m, n, temp = 0);
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов= {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+}
+
